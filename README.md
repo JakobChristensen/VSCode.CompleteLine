91d2804b9bef4f7e07f02c08229e4a980fce0d2e
JakobChristensen <refactor11@gmail.com>
2018-12-17 13:59:25 +0100

    Tweaks and optimizations

--- README.md
+++ README.md
@@ -1,16 +1,98 @@
-# Complete Line extension for VS Code
+# Complete Line
 
-This extension completes lines of code with regards to parentheses, curly braces and semi-colons.
+For partial lines of code, this extension completes the line with parentheses, curly braces and semi-colons.
 
-## Features
+For blank lines, it provides suggestions for a new line of code based on the previous line of code.
 
-This extension completes lines of code with regards to parentheses, curly braces and semi-colons.
+Only C-like languages (JavaScript, Typescript, C#, C and C++) are supported. The extension does not do
+proper language analysis, but uses indentation to figure out code structure.
 
-Calling out known issues can help limit users opening duplicate issues against your extension.
+This extension is heavily inspired by the Complete Statement functionality in JetBrains Resharper.
+
+## Complete a partial line of code
+
+If the caret is located on a non-blank line, the extension will try to append parentheses, curly braces and 
+semi-colons to make a valid line of code.
+
+Suppose the caret is located on this line of code:
+```
+if (text.startsWith('A'
+```
+
+If you press Ctrl+Shift+Enter, the extension will insert two parentheses and a pair of curly braces and 
+position the caret inside the braces:
+
+```
+if (text.startsWith('A')) {
+
+}
+```
+
+## Complete a blank line
+
+If the caret is located on a blank line, the extension suggests a new line of code based on the previous 
+line. 
+
+Suppose the caret is located on the blank line after this assigment statement.
+```
+const startsWithA = text.startsWith('A');
+
+```
+If you press Ctrl+Alt+Enter, the extension will suggest an `if` statement (and other statements), resulting in this code:
+
+```
+const startsWithA = text.startsWith('A');
+if (startsWithA) {
+
+}
+```
+
+Suppose you press Ctrl+Alt+Enter again inside the curly braces, the extension will suggest a `return` statement, 
+resulting in this code:
+```
+const startsWithA = text.startsWith('A');
+if (startsWithA) {
+    return;
+}
+```
+
+Inside loop statements (`for`, `foreach`, `while`, `do`) , the extension suggests `break` and `continue` statements.
+
+```
+for (let i = 0; i < text.length; i++) {
+    if (i > 10) {
+
+    }
+}
+```
+When you press Ctrl+Alt+Enter and select `break`.
+```
+for (let i = 0; i < text.length; i++) {
+    if (i > 10) {
+        break;        
+    }
+}
+```
+
+PLEASE NOTICE: The extension does not perform proper language analysis. Instead it uses indentation to figure
+out the code structure. If you uses 'special' indentation rules or like to break code across multiple lines,
+the extention will probably not work as intended.
+
+## Rules
+
+* After an assignment, suggest an `if (...)` statement
+* After an assignment, suggest a `for (let index of ...)` statement
+* After an assignment, suggest a `foreach (var index in...)` statement (C# only)
+* After an `if` statement, suggest an `else` clause
+* Inside a `switch` statement, suggest a `case` clause
+* Inside a `switch` statement, suggest a `default` clause
+* After a `try` statement, suggest a `catch` clause
+* At the last line of a block, suggest a `return` statement
+* Inside a loop and at the last line of a block, suggest a `break` statement
+* Inside a loop and at the last line of a block, suggest a `continue` statement
 
 ## Release Notes
 
 ### 1.0.0
 
 Initial release.
-
--- src/extension.ts
+++ src/extension.ts
@@ -24,7 +24,12 @@ interface SnippetQuickPickItem extends vscode.QuickPickItem {
     snippet: vscode.SnippetString;
 }
 
-const blockKeywords = ["if", "while", "for", "foreach", "do", "function", "else", "class", "switch", "try", "catch", "with"];
+enum AdjacentLine {
+    Previous = -1,
+    Next = 1
+}
+
+const blockKeywords = ["if", "for", "foreach", "while", "do", "function", "else", "class", "switch", "try", "catch", "with"];
 
 const loopKeywords = ["for", "foreach", "do", "while"];
 
@@ -32,47 +37,65 @@ const suggestions: Array<Suggestion> = [
     // if (...)
     {
         label: "if (@1)...",
-        snippet: "if (@1) {\r\t$1\r}",
+        snippet: "if (@1) {\n\t$0\n}",
+        languages: ["javascript", "javascriptreact", "typescript", "typescriptreact"],
         when: c => c.previousLineText.match("^let\\s+(\\w+)\\s*=.*")
     },
     {
         label: "if (@1)...",
-        snippet: "if (@1) {\r\t$1\r}",
+        snippet: "if (@1) {\n\t$0\n}",
+        languages: ["javascript", "javascriptreact", "typescript", "typescriptreact"],
         when: c => c.previousLineText.match("^const\\s+(\\w+)\\s*=.*")
     },
     {
         label: "if (@1)...",
-        snippet: "if (@1) {\r\t$1\r}",
+        snippet: "if (@1) {\n\t$0\n}",
+        languages: ["javascript", "javascriptreact", "typescript", "typescriptreact"],
         when: c => c.previousLineText.match("^var\\s+(\\w+)\\s*=.*")
     },
     {
         label: "if (@1)...",
-        snippet: "if (@1) {\r\t$1\r}",
+        snippet: "if (@1) {\n\t$0\n}",
+        languages: ["javascript", "javascriptreact", "typescript", "typescriptreact"],
+        when: c => c.previousLineText.match("^(\\w+)\\s*=.*")
+    },
+
+    // if (...) [csharp]
+    {
+        label: "if (@1 == null)...",
+        snippet: "if (@1 == null) {\n\t$0\n}",
+        languages: ["csharp"],
+        when: c => c.previousLineText.match("^var\\s+(\\w+)\\s*=.*")
+    },
+    {
+        label: "if (@1 == null)...",
+        snippet: "if (@1 == null) {\n\t$0\n}",
+        languages: ["csharp"],
         when: c => c.previousLineText.match("^(\\w+)\\s*=.*")
     },
 
     // for (...) [javascript, typescript]
     {
         label: "for (let index of @1)...",
-        snippet: "for (let ${1:index} of @1) {\r\t$2\r}",
+        snippet: "for (let ${1:index} of @1) {\n\t$0\n}",
         languages: ["javascript", "javascriptreact", "typescript", "typescriptreact"],
         when: c => c.previousLineText.match("^let\\s+(\\w+)\\s*=.*")
     },
     {
         label: "for (let index of @1)...",
-        snippet: "for (let ${1:index} of @1) {\r\t$2\r}",
+        snippet: "for (let ${1:index} of @1) {\n\t$0\n}",
         languages: ["javascript", "javascriptreact", "typescript", "typescriptreact"],
         when: c => c.previousLineText.match("^const\\s+(\\w+)\\s*=.*")
     },
     {
         label: "for (let index of @1)...",
-        snippet: "for (let ${1:index} of @1) {\r\t$2\r}",
+        snippet: "for (let ${1:index} of @1) {\n\t$0\n}",
         languages: ["javascript", "javascriptreact", "typescript", "typescriptreact"],
         when: c => c.previousLineText.match("^var\\s+(\\w+)\\s*=.*")
     },
     {
         label: "for (let index of @1)...",
-        snippet: "for (let ${1:index} of @1) {\r\t$2\r}",
+        snippet: "for (let ${1:index} of @1) {\n\t$0\n}",
         languages: ["javascript", "javascriptreact", "typescript", "typescriptreact"],
         when: c => c.previousLineText.match("^(\\w+)\\s*=.*")
     },
@@ -80,13 +103,13 @@ const suggestions: Array<Suggestion> = [
     // foreach (...) [csharp]
     {
         label: "foreach (var index in @1)...",
-        snippet: "foreach (var ${1:index} in @1) {\r\t$2\r}",
+        snippet: "foreach (var ${1:index} in @1) {\n\t$0\n}",
         languages: ["csharp"],
         when: c => c.previousLineText.match("^var\\s+(\\w+)\\s*=.*")
     },
     {
         label: "foreach (var index in @1)...",
-        snippet: "foreach (var ${1:index} in @1) {\r\t$2\r}",
+        snippet: "foreach (var ${1:index} in @1) {\n\t$0\n}",
         languages: ["csharp"],
         when: c => c.previousLineText.match("^(\\w+)\\s*=.*")
     },
@@ -94,22 +117,22 @@ const suggestions: Array<Suggestion> = [
     // more suggestions
     {
         label: "else...",
-        snippet: "else {\r\t$1\r}",
+        snippet: "else {\n\t$0\n}",
         when: c => c.previousLineKeyword === "if"
     },
     {
         label: "case...",
-        snippet: "case ${1:condition}:\r\t$2\r\tbreak;",
+        snippet: "case ${1:condition}:\n\t$0\n\tbreak;",
         when: c => c.parentLineKeyword === "switch" || c.previousLineKeyword === "case"
     },
     {
         label: "default...",
-        snippet: "default:\r\t$1\r\tbreak;",
+        snippet: "default:\n\t$0\n\tbreak;",
         when: c => c.parentLineKeyword === "switch" || c.previousLineKeyword === "case"
     },
     {
         label: "catch...",
-        snippet: "catch {\r\t$1\r}",
+        snippet: "catch {\n\t$0\n}",
         when: c => c.previousLineKeyword === "try"
     },
     {
@@ -141,7 +164,7 @@ function completeLine(): void {
     if (!editor) {
         return;
     }
-       
+
     if (supportedLanguages.indexOf(editor.document.languageId) < 0) {
         return;
     }
@@ -151,40 +174,42 @@ function completeLine(): void {
         return;
     }
 
-    if (line.text.trim() === "") {
-        completeEmptyLine(editor);
+    if (line.isEmptyOrWhitespace) {
+        completeBlankLine(editor);
     }
     else {
         completePartialLine(editor);
     }
 }
 
-function completeEmptyLine(editor: vscode.TextEditor): void {
-    const previousLine = getPreviousLine(editor.document, editor.selection.active.line);
-    const nextLine = getNextLine(editor.document, editor.selection.active.line);
-    const parentLines = getParentLines(editor.document, editor.selection.active.line);
+function completeBlankLine(editor: vscode.TextEditor): void {
+    const document = editor.document;
+    const activeLine = editor.selection.active.line;
+    const language = document.languageId;
+
+    const previousLine = getAdjacentLine(document, activeLine, AdjacentLine.Previous);
+    const nextLine = getAdjacentLine(document, activeLine, AdjacentLine.Next);
+    const parentLines = getParentLines(document, activeLine);
 
     const context: SuggestionContext = {
         previousLineKeyword: previousLine ? previousLine.keyword : "",
-        previousLineText: previousLine ? previousLine.trimmedLine : "",
+        previousLineText: previousLine ? previousLine.line.text.trim() : "",
         parentLineKeyword: parentLines.length > 0 ? parentLines[0].keyword : "",
-        parentLineText: parentLines.length > 0 ? parentLines[0].trimmedLine : "",
+        parentLineText: parentLines.length > 0 ? parentLines[0].line.text.trim() : "",
         isWithinLoop: isWithinLoop(parentLines),
         previousLine: previousLine,
         parentLines: parentLines,
         nextLine: nextLine,
     };
 
-    const language = editor.document.languageId;
-
-    let items = new Array<SnippetQuickPickItem>();
+    const items = new Array<SnippetQuickPickItem>();
 
     for (let suggestion of suggestions) {
-        if (suggestion.languages && suggestion.languages.indexOf(language) < 0)   {
+        if (suggestion.languages && suggestion.languages.indexOf(language) < 0) {
             continue;
         }
 
-        var result = suggestion.when(context);
+        const result = suggestion.when(context);
         if (!result) {
             continue;
         }
@@ -192,12 +217,17 @@ function completeEmptyLine(editor: vscode.TextEditor): void {
         let label = suggestion.label;
         let snippet = suggestion.snippet;
 
+        // if regex matches, replace parameters (@1, @2, ...) with values from the regex
         if (Array.isArray(result)) {
-            for (let index = 1; index < result.length; index++) {
-                let parameter = result[index];
-                label = label.replace("@" + index, parameter);
-                snippet = snippet.replace("@" + index, parameter);
+            for (let i = 1; i < result.length; i++) {
+                const parameter = result[i];
+                label = label.replace("@" + i, parameter);
+                snippet = snippet.replace("@" + i, parameter);
             }
+        }
+
+        if (document.eol === vscode.EndOfLine.CRLF) {
+            snippet = snippet.replace("\n", "\r\n");
         }
 
         items.push({ label: label, snippet: new vscode.SnippetString(snippet) });
@@ -211,153 +241,93 @@ function completeEmptyLine(editor: vscode.TextEditor): void {
         return;
     }
 
-    vscode.window.showQuickPick<SnippetQuickPickItem>(items).then(selectedItem => {
-        if (selectedItem !== undefined) {
+    vscode.window.showQuickPick<SnippetQuickPickItem>(items, { placeHolder: "Suggestions" }).then(selectedItem => {
+        if (selectedItem) {
             editor.insertSnippet(selectedItem.snippet);
         }
     });
 }
 
 function completePartialLine(editor: vscode.TextEditor): void {
-    let line = editor.document.lineAt(editor.selection.active.line);
-    let trimmedLine = line ? line.text.trim() : "";
-    let nextLine = editor.document.lineAt(editor.selection.active.line + 1);
-    let nextTrimmedLine = nextLine ? nextLine.text.trim() : "";
+    const document = editor.document;
+    const activeLine = editor.selection.active.line;
 
-    let code = "";
-    let position: vscode.Position | undefined = undefined;
-    let indent = getIndent(line.text);
+    const line = document.lineAt(activeLine);
+    const trimmedLineText = line ? line.text.trim() : "";
 
-    if (blockKeywords.some(keyword => trimmedLine.startsWith(keyword)) && !(trimmedLine.endsWith("{") || nextTrimmedLine.startsWith("{"))) {
-        code = getParentheses(trimmedLine) + " {\r" + indent.nestedSpaces + "\r" + indent.spaces + "}";
-        position = new vscode.Position(editor.selection.active.line + 1, indent.nestedPosition);
+    let nextTrimmedLineText = "";
+    if (activeLine < document.lineCount - 1) {
+        const nextLine = document.lineAt(activeLine + 1);
+        nextTrimmedLineText = nextLine ? nextLine.text.trim() : "";
     }
-    else if (trimmedLine.endsWith("{")) {
-        code = "\r" + indent.nestedSpaces + "\r" + indent.spaces + "}";
-        position = new vscode.Position(editor.selection.active.line + 1, indent.nestedPosition);
+
+    let snippet = "";
+
+    if (blockKeywords.some(keyword => trimmedLineText.startsWith(keyword)) && !(trimmedLineText.endsWith("{") || nextTrimmedLineText.startsWith("{"))) {
+        snippet = getClosingParentheses(line) + " {\n\t$0\n}";
     }
-    else if (trimmedLine.endsWith("=>")) {
-        code = " {\r" + indent.nestedSpaces + "\r" + indent.spaces + "}";
-        position = new vscode.Position(editor.selection.active.line + 1, indent.nestedPosition);
+    else if (trimmedLineText.endsWith("{")) {
+        snippet = "\n\t$0\n}";
     }
-    else if (!trimmedLine.endsWith(";")) {
-        code = getParentheses(trimmedLine) + ";\r" + indent.spaces;
-        position = new vscode.Position(editor.selection.active.line + 1, indent.position);
+    else if (trimmedLineText.endsWith("=>")) {
+        snippet = " {\n\t$0\n}";
+    }
+    else if (!trimmedLineText.endsWith(";")) {
+        snippet = getClosingParentheses(line) + ";\n";
     }
     else {
-        code = "\r" + indent.spaces;
-        position = new vscode.Position(editor.selection.active.line + 1, indent.position);
+        snippet = "\n";
     }
 
-    if (code === "") {
+    if (snippet === "") {
         return;
     }
 
-    editor.edit(edit => {
-        edit.replace(line.range, line.text + code);
-    }).then(() => {
-        if (position && editor) {
-            editor.selection = new vscode.Selection(position, position);
-        }
-    });
-}
-
-function getIndent(line: string): { spaces: string, position: number, nestedSpaces: string, nestedPosition: number } {
-    const tabSize = vscode.workspace.getConfiguration("editor", null).get<number>("tabSize", 4);
-    let spaces = "";
-    let position = 0;
-
-    for (let i = 0; i < line.length; i++) {
-        const c = line.charAt(i);
-        if (c !== " " && c !== "\t") {
-            break;
-        }
-
-        spaces += c;
-        position += (c === "\t" ? tabSize : 1);
+    if (document.eol === vscode.EndOfLine.CRLF) {
+        snippet = snippet.replace("\n", "\r\n");
     }
 
-    let nestedPosition = position + tabSize;
-    let nestedSpaces = spaces + " ".repeat(tabSize);
-
-    return { spaces, position, nestedSpaces, nestedPosition };
+    editor.insertSnippet(new vscode.SnippetString(snippet), line.range.end);
 }
 
-function getIndentLevel(line: string): number {
-    const tabSize = vscode.workspace.getConfiguration("editor", null).get<number>("tabSize", 4);
-    let level = 0;
+function getClosingParentheses(line: vscode.TextLine): string {
+    const trimmedLineText = line.text.trim();
+    let openParentheses = 0;
 
-    for (let i = 0; i < line.length; i++) {
-        const c = line.substr(i, 1);
-        if (c === " ") {
-            level++;
-        }
-        else if (c === "\t") {
-            level += tabSize;
-        }
-        else {
-            break;
-        }
-    }
-
-    return level;
-}
-
-function getNextLine(document: vscode.TextDocument, activeLine: number): Line | undefined {
-    const indentLevel = getIndentLevel(document.lineAt(activeLine).text);
-
-    for (let lineNumber = activeLine + 1; lineNumber < document.lineCount; lineNumber++) {
-        let line = document.lineAt(lineNumber).text;
-        if (!isStatement(line)) {
-            continue;
-        }
-
-        let level = getIndentLevel(line);
-        if (level < indentLevel) {
-            return undefined;
-        }
-
-        if (level === indentLevel) {
-            return new Line(line);
-        }
-    }
-
-    return undefined;
-}
-
-function getParentheses(line: string): string {
-    let open = 0;
-
-    for (let i = 0; i < line.length; i++) {
-        switch (line.charAt(i)) {
+    for (let i = 0; i < trimmedLineText.length; i++) {
+        switch (trimmedLineText.charAt(i)) {
             case '(':
-                open++;
+                openParentheses++;
                 break;
             case ')':
-                open--;
+                openParentheses--;
                 break;
         }
     }
 
-    return open > 0 ? ")".repeat(open) : "";
+    return openParentheses > 0 ? ")".repeat(openParentheses) : "";
 }
 
-function getPreviousLine(document: vscode.TextDocument, activeLine: number): Line | undefined {
-    const indentLevel = getIndentLevel(document.lineAt(activeLine).text);
+function getLineLevel(line: vscode.TextLine): number {
+    return line.firstNonWhitespaceCharacterIndex;
+}
 
-    for (let lineNumber = activeLine - 1; lineNumber >= 0; lineNumber--) {
-        const line = document.lineAt(lineNumber).text;
+function getAdjacentLine(document: vscode.TextDocument, activeLine: number, direction: AdjacentLine): Line | undefined {
+    const activeLineLevel = getLineLevel(document.lineAt(activeLine));
+
+    for (let lineNumber = activeLine + direction; lineNumber >= 0 && lineNumber < document.lineCount; lineNumber += direction) {
+        const line = document.lineAt(lineNumber);
         if (!isStatement(line)) {
             continue;
         }
 
-        const level = getIndentLevel(line);
-        if (level < indentLevel) {
-            return undefined;
+        const lineLevel = getLineLevel(line);
+        if (lineLevel === activeLineLevel) {
+            return new Line(line);
         }
 
-        if (level === indentLevel) {
-            return new Line(line);
+        if (lineLevel < activeLineLevel) {
+            return undefined;
         }
     }
 
@@ -365,45 +335,41 @@ function getPreviousLine(document: vscode.TextDocument, activeLine: number): Lin
 }
 
 function getParentLines(document: vscode.TextDocument, activeLine: number): Line[] {
-    let codeLines = new Array<Line>();
-    let indentLevel = getIndentLevel(document.lineAt(activeLine).text);
+    let lines = new Array<Line>();
+    let activeLineLevel = getLineLevel(document.lineAt(activeLine));
 
     for (let lineNumber = activeLine - 1; lineNumber >= 0; lineNumber--) {
-        let line = document.lineAt(lineNumber).text;
+        const line = document.lineAt(lineNumber);
         if (!isStatement(line)) {
             continue;
         }
 
-        let level = getIndentLevel(line);
-        if (level >= indentLevel) {
-            continue;
+        let lineLevel = getLineLevel(line);
+        if (lineLevel < activeLineLevel) {
+            lines.push(new Line(line));
+            activeLineLevel = lineLevel;
         }
-
-        let codeLine = new Line(line);
-        codeLines.push(codeLine);
-
-        indentLevel = level;
     }
 
-    return codeLines;
+    return lines;
 }
 
-function isStatement(line: string): boolean {
-    const trimmedLine = line.trim();
-
+function isStatement(line: vscode.TextLine): boolean {
     // empty line
-    if (trimmedLine === "") {
+    if (line.isEmptyOrWhitespace) {
         return false;
     }
 
-    // todo: support block comments
+    const trimmedLineText = line.text.trim();
+
     // comments
-    if (trimmedLine.startsWith("//") || trimmedLine.startsWith("/*") || trimmedLine.startsWith("*/") || trimmedLine.startsWith("*")) {
+    // todo: support for block comments
+    if (trimmedLineText.startsWith("//") || trimmedLineText.startsWith("/*") || trimmedLineText.startsWith("*/") || trimmedLineText.startsWith("*")) {
         return false;
     }
 
     // curly braces
-    if (trimmedLine === "{" || trimmedLine === "}") {
+    if (trimmedLineText === "{" || trimmedLineText === "}") {
         return false;
     }
 
@@ -411,29 +377,17 @@ function isStatement(line: string): boolean {
 }
 
 function isWithinLoop(parentLines: Line[]): boolean {
-    for (let line of parentLines) {
-        if (loopKeywords.indexOf(line.keyword) >= 0) {
-            return true;
-        }
-    }
-
-    return false;
+    return parentLines.some(line => loopKeywords.indexOf(line.keyword) >= 0);
 }
 
 class Line {
     private static keywordRegExp = new RegExp("^\\w+");
 
-    public indent: number = 0;
     public keyword: string = "";
-    public trimmedLine: string = "";
 
-    constructor(public line: string) {
-        this.trimmedLine = line.trim();
-
-        let result = Line.keywordRegExp.exec(this.trimmedLine);
+    constructor(public line: vscode.TextLine) {
+        const result = Line.keywordRegExp.exec(line.text.trim());
         this.keyword = result ? result[0] : "";
-
-        this.indent = getIndentLevel(line);
     }
 }
 
